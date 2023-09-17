b6ce1aa6f4b44e72eb956cd49f87b0a79a4d27bf
washington-shoji <shoji.washington@gmail.com>
2023-09-17 17:09:16 +1000

    -updated main

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/washington-shoji/gobare/server"
+import (
+	"log"
+
+	"github.com/washington-shoji/gobare/databases"
+	"github.com/washington-shoji/gobare/server"
+)
 
 func main() {
+
+	store, err := databases.PostgresDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := store.Init(); err != nil {
+		log.Fatal(err)
+	}
 
 	server := server.Server(":3030")
 	server.Run()
