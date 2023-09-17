cfddc350ee5951de6abdbb5071d4ebdfcbced110
washington-shoji <shoji.washington@gmail.com>
2023-09-17 17:06:11 +1000

    -added getId helper

---
+++ helpers/get_id.go
@@ -0,0 +1,20 @@
+package helpers
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+func GetID(r *http.Request) (int, error) {
+	idString := mux.Vars(r)["id"]
+
+	id, err := strconv.Atoi(idString)
+	if err != nil {
+		return id, fmt.Errorf("invalid id given %s", idString)
+	}
+
+	return id, nil
+}
