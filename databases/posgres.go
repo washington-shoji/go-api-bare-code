69685e78d99aa0e81374f2c713e4884812d12f58
washington-shoji <shoji.washington@gmail.com>
2023-09-17 17:05:05 +1000

    -added postgres database

---
+++ databases/posgres.go
@@ -0,0 +1,41 @@
+package databases
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+
+	"github.com/washington-shoji/gobare/configs"
+)
+
+type PostgresStore struct {
+	db *sql.DB
+}
+
+var DB PostgresStore
+
+func PostgresDB() (*PostgresStore, error) {
+	connStr := fmt.Sprintf("user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		configs.EnvConfig("DB_USER"), configs.EnvConfig("DB_PASSWORD"), configs.EnvConfig("DB_NAME"))
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	DB = PostgresStore{
+		db: db,
+	}
+
+	return &PostgresStore{
+		db: db,
+	}, nil
+}
+
+func (store *PostgresStore) Init() error {
+	return store.CreateTables()
+}
