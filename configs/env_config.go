bdb295eafc3b5ba398e63ee2a8aa4be76b5fdb3e
washington-shoji <shoji.washington@gmail.com>
2023-09-17 17:03:41 +1000

    -added env config

---
+++ configs/env_config.go
@@ -0,0 +1,17 @@
+package configs
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func EnvConfig(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Print("Error loading .env file")
+	}
+	return os.Getenv(key)
+
+}
