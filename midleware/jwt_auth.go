56499bf6f756441e429a6c12b685d59288d49459
washington-shoji <shoji.washington@gmail.com>
2023-09-18 18:19:43 +1000

    -added jwt auth middleware

---
+++ midleware/jwt_auth.go
@@ -0,0 +1,77 @@
+package midleware
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/washington-shoji/gobare/configs"
+	"github.com/washington-shoji/gobare/databases"
+	"github.com/washington-shoji/gobare/helpers"
+)
+
+func JWTAuth(handlerFunc http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("calling JWT auth middleware")
+
+		tokenString := r.Header.Get("Authorization")
+		token, err := validateJWT(tokenString)
+		if err != nil {
+			permissionDenied(w)
+			return
+		}
+
+		if !token.Valid {
+			permissionDenied(w)
+			return
+		}
+
+		userID, err := helpers.GetID(r)
+		if err != nil {
+			permissionDenied(w)
+			return
+		}
+		account, err := databases.GetAccountByID(userID)
+		if err != nil {
+			permissionDenied(w)
+			return
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+		if account.UserName != claims["userName"] {
+			permissionDenied(w)
+			return
+		}
+
+		if err != nil {
+			permissionDenied(w)
+			return
+		}
+
+		handlerFunc(w, r)
+	}
+}
+
+func validateJWT(tokenStr string) (*jwt.Token, error) {
+	secret := configs.EnvConfig("JWT_SECRET")
+
+	return jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		res, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		fmt.Println("res", res)
+		fmt.Println("ok", ok)
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte(secret), nil
+	})
+
+}
+
+func permissionDenied(w http.ResponseWriter) {
+	helpers.WriteJson(w, http.StatusForbidden, databases.ApiError{Error: "permission denied"})
+}
