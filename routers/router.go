892b3c8d1817f4f0c97c6297672748ca7bbb8f64
washington-shoji <shoji.washington@gmail.com>
2023-09-20 18:28:10 +1000

    -updated the router

--- routers/router.go
+++ routers/router.go
@@ -4,14 +4,20 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/washington-shoji/gobare/handlers"
 	"github.com/washington-shoji/gobare/helpers"
+	"github.com/washington-shoji/gobare/middleware"
 )
 
 func SetupRoutes() *mux.Router {
 	router := mux.NewRouter()
 
+	router.HandleFunc("/login", helpers.MakeHTTPHandleFunc(handlers.HandleLogin))
 	router.HandleFunc("/health-check", helpers.MakeHTTPHandleFunc(handlers.HandleHealthCheck))
 	router.HandleFunc("/account", helpers.MakeHTTPHandleFunc(handlers.HandleAccount))
-	router.HandleFunc("/account/{id}", helpers.MakeHTTPHandleFunc(handlers.HandleAccountByID))
+	router.HandleFunc("/account/{id}", middleware.JWTAuth(helpers.MakeHTTPHandleFunc(handlers.HandleAccountByID)))
+	router.HandleFunc("/author", helpers.MakeHTTPHandleFunc(handlers.HandleAuthor))
+	router.HandleFunc("/author/{id}", helpers.MakeHTTPHandleFunc(handlers.HandleAuthorByID))
+	router.HandleFunc("/book", helpers.MakeHTTPHandleFunc(handlers.HandleBook))
+	router.HandleFunc("/book/{id}", helpers.MakeHTTPHandleFunc(handlers.HandleBookByID))
 
 	return router
 }
